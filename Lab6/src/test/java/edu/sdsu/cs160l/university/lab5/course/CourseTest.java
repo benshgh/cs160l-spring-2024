b71a49e81c72f9232ca4d90ce1e6377b4d928ae6
John Pateros <jpateros5410@sdsu.edu>
2024-02-26 20:37:25 -0800

    updated unit tests for Lab6 singleton implementation

--- Lab6/src/test/java/edu/sdsu/cs160l/university/lab5/course/CourseTest.java
+++ Lab6/src/test/java/edu/sdsu/cs160l/university/lab5/course/CourseTest.java
@@ -8,32 +8,34 @@ import edu.sdsu.cs160l.university.lab5.course.CourseFactory;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CourseTest {
+
+    //TODO: Comment out these test cases and run the unit tests once you have implemnetd the singleton classes for CS 150 - CS340
     @Test
     public void testInstanceCS150(){
-        Course c1 = CourseFactory.getCourse("CS150");
-        Course c2 = CourseFactory.getCourse("CS150");
-        assertEquals(c1, c2);
+//        Course c1 = CS150.getInstance();
+//        Course c2 = CS150.getInstance();
+//        assertEquals(c1, c2);
     }
 
     @Test
     public void testInstanceCS160(){
-        Course c1 = CourseFactory.getCourse("CS160");
-        Course c2 = CourseFactory.getCourse("CS160");
-        assertEquals(c1, c2);
+//        Course c1 = CS160.getInstance();
+//        Course c2 = CS160.getInstance();
+//        assertEquals(c1, c2);
     }
 
     @Test
     public void testInstanceCS210(){
-        Course c1 = CourseFactory.getCourse("CS210");
-        Course c2 = CourseFactory.getCourse("CS210");
-        assertEquals(c1, c2);
+//        Course c1 = CS210.getInstance();
+//        Course c2 = CS210.getInstance();
+//        assertEquals(c1, c2);
     }
 
     @Test
     public void testInstance340(){
-        Course c1 = CourseFactory.getCourse("CS340");
-        Course c2 = CourseFactory.getCourse("CS340");
-        assertEquals(c1, c2);
+//        Course c1 = CS340.getInstance();
+//        Course c2 = CS340.getInstance();
+//        assertEquals(c1, c2);
     }
 
 }
